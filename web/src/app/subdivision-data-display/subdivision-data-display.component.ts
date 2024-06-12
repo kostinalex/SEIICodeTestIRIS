248b7116e8c2a8705a13deef9abb1d7944b201d6
kostinalex <kostin2607@gmail.com>
2024-06-12 16:17:39 -0400

    test call to the api

--- web/src/app/subdivision-data-display/subdivision-data-display.component.ts
+++ web/src/app/subdivision-data-display/subdivision-data-display.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-subdivision-data-display',
-  templateUrl: './subdivision-data-display.component.html',
-  styleUrls: ['./subdivision-data-display.component.css']
+  selector: "app-subdivision-data-display",
+  templateUrl: "./subdivision-data-display.component.html",
+  styleUrls: ["./subdivision-data-display.component.css"],
 })
 export class SubdivisionDataDisplayComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    console.log("====>home");
+    this.http.get(environment.api + "/v1/subdivisions").subscribe((resp) => {
+      console.log("====>resp", resp);
+    });
   }
-
 }
