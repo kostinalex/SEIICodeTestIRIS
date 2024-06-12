91f6cff6b3c3e7a6e263bf5a27b88aec4578c23e
kostinalex <kostin2607@gmail.com>
2024-06-12 16:15:36 -0400

    cors added to api

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const subdivisions = require('./subdivision.json')
+const express = require("express");
+const app = express();
+const port = 3000;
+const subdivisions = require("./subdivision.json");
+const cors = require("cors");
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors());
 
-app.get('/v1/subdivisions', (req, res) => {
-    res.send(subdivisions);
-})
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => {
-    console.log('Example app listening on port 3000!')
+app.get("/v1/subdivisions", (req, res) => {
+  res.send(subdivisions);
 });
 
+app.listen(port, () => {
+  console.log("Example app listening on port 3000!");
+});
